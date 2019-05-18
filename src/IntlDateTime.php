52323b2298274cd3ebcf34bff9e996a212b918a9
Klemen Tušar <techouse@gmail.com>
2019-05-18 13:44:01 +0200

    Bugfix Allow clearing dates

--- src/IntlDateTime.php
+++ src/IntlDateTime.php
@@ -62,8 +62,8 @@ class IntlDateTime extends DateTime
      */
     protected function fillAttributeFromRequest(NovaRequest $request, $requestAttribute, $model, $attribute)
     {
-        if ($request->exists($requestAttribute) && $request[$requestAttribute]) {
-            $model->{$attribute} = Carbon::parse($request[$requestAttribute]);
+        if ($request->exists($requestAttribute)) {
+            $model->{$attribute} = $request[$requestAttribute] ? Carbon::parse($request[$requestAttribute]) : null;
         }
     }
 

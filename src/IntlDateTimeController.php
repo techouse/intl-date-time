725970b537e6a111d8dd5d6a930f80dceb10805c
Klemen Tušar <techouse@gmail.com>
2019-11-07 21:39:39 +0100

    :bug: Swap App Controller for Illuminate\Routing\Controller

--- src/IntlDateTimeController.php
+++ src/IntlDateTimeController.php
@@ -2,7 +2,7 @@
 
 namespace Techouse\IntlDateTime;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Support\Facades\File;
 use Laravel\Nova\Http\Requests\NovaRequest;

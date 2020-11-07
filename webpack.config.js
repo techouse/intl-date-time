ef44a5d986aa6bf77acf8c8f6094cf84033d92bf
Klemen Tušar <techouse@gmail.com>
2020-11-07 10:21:46 +0000

    :arrow_up: Updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,82 +1,80 @@
 const path                    = require("path"),
       outputPath              = path.resolve(__dirname, "dist"),
-      {CleanWebpackPlugin}    = require("clean-webpack-plugin"),
+      { CleanWebpackPlugin }  = require("clean-webpack-plugin"),
       MiniCssExtractPlugin    = require("mini-css-extract-plugin"),
       Fiber                   = require("fibers"),
-      {VueLoaderPlugin}       = require("vue-loader"),
+      { VueLoaderPlugin }     = require("vue-loader"),
       OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin"),
       TerserPlugin            = require("terser-webpack-plugin"),
       ManifestPlugin          = require("webpack-manifest-plugin"),
       env                     = process.env.NODE_ENV,
-      npm_config_argv         = JSON.parse(process.env.npm_config_argv),
-      isWatch                 = npm_config_argv.remain.some(el => el.startsWith("--watch")),
+      isWatch                 = process.env.npm_lifecycle_event === "watch",
       sourceMap               = env !== "production",
       production              = env === "production",
       webpack                 = require("webpack")
 
 const config = {
-    mode:         env,
-    target:       "web",
-    entry:        {
+    mode: env,
+    target: "web",
+    entry: {
         field: [
             "./resources/js/field.js",
             "./resources/sass/field.scss"
         ]
     },
-    output:       {
-        path:          outputPath,
-        publicPath:    "/nova-vendor/intl-date-time/",
-        filename:      "js/[name].js",
+    output: {
+        path: outputPath,
+        publicPath: "/nova-vendor/intl-date-time/",
+        filename: "js/[name].js",
         chunkFilename: "js/[name].js",
         jsonpFunction: "wpJsonpIntlDateTime"
     },
     optimization: {},
-    resolve:      {
-        alias:      {
+    resolve: {
+        alias: {
             "vue$": "vue/dist/vue.esm.js"
         },
         extensions: ["*", ".js", ".vue", ".json"],
-        modules:    ["./node_modules",
-                     "./resources/js/components",]
+        modules: ["./node_modules",
+                  "./resources/js/components",]
     },
-    stats:        {
+    stats: {
         colors: true
     },
-    devtool:      sourceMap ? "cheap-module-eval-source-map" : undefined,
-    module:       {
+    devtool: sourceMap ? "cheap-module-eval-source-map" : undefined,
+    module: {
         rules: [
             {
-                test:    /\.m?js$/,
-                loader:  "babel-loader"
+                test: /\.m?js$/,
+                loader: "babel-loader"
             },
             {
-                test:    /\.vue$/,
-                loader:  "vue-loader"
+                test: /\.vue$/,
+                loader: "vue-loader"
             },
             {
                 test: /\.(sa|sc|c)ss$/i,
-                use:  [
+                use: [
                     {
-                        loader:  MiniCssExtractPlugin.loader,
-                        options: {sourceMap}
+                        loader: MiniCssExtractPlugin.loader,
                     },
                     {
-                        loader:  "css-loader",
+                        loader: "css-loader",
                         options: {
                             sourceMap,
                             importLoaders: 2
                         }
                     },
-                    {loader: "postcss-loader", options: {sourceMap}},
+                    { loader: "postcss-loader", options: { sourceMap } },
                     "resolve-url-loader",
                     {
-                        loader:  "sass-loader",
+                        loader: "sass-loader",
                         options: {
                             sourceMap,
                             implementation: require("sass"),
-                            sassOptions:    {
-                                fiber:        Fiber,
-                                indentWidth:  4,
+                            sassOptions: {
+                                fiber: Fiber,
+                                indentWidth: 4,
                                 includePaths: [path.resolve(__dirname, "resources/scss")],
                             },
                         }
@@ -84,13 +82,12 @@ const config = {
             }
         ]
     },
-    plugins:      [
+    plugins: [
         new webpack.ProgressPlugin(),
         new VueLoaderPlugin(),
-        new CleanWebpackPlugin({cleanStaleWebpackAssets: !isWatch}),
+        new CleanWebpackPlugin({ cleanStaleWebpackAssets: !isWatch }),
         new MiniCssExtractPlugin({
-            path:          outputPath + "/css",
-            filename:      "css/[name].css",
+            filename: "css/[name].css",
             chunkFilename: "css/[name].css"
         }),
         new ManifestPlugin({
@@ -103,7 +100,7 @@ if (production) {
     config.optimization.minimizer = [
         new OptimizeCSSAssetsPlugin(),
         new TerserPlugin({
-            cache:    true,
+            cache: true,
             parallel: true,
         }),
     ]
